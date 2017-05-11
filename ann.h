e7a7742058a3edd23b274671eb8a245591b1e4be
Grif Hughes <ghughes@smu.edu>
2017-05-11 02:01:09 -0500

    maybe work

--- ann.h
+++ ann.h
@@ -1,9 +1,26 @@
-#ifndef ANN_H
-#define ANN_H
+struct ann {
+        struct layer *hid, *out;
+        float *dtmp;
+        int batch_size;
+};      
 
-#include "layer.h"
+struct ann *ann_create(int f, int h, int cl, int bs)
+{
+        struct ann *a;
+        cudaMallocManaged(&a, sizeof(struct ann));
+        cudaMalloc((void **)&(a->dtmp), sizeof(float) * h * bs);
+        a->hid = layer_create(h, f, bs);
+        a->out = layer_create(cl, h, bs);
+        a->batch_size = bs;
+        return a;
+}
+
+void ann_destroy(struct ann *a)
+{
+        layer_destroy(a->out);
+        layer_destroy(a->hid);
+        cudaFree(a->dtmp);
+        cudaFree(a);
+}
 
 
-
-
-#endif
--- test.cu
+++ test.cu
@@ -21,31 +21,6 @@ const float neg = -1.0f;
 const float alph = 1.0f;
 const float bet = 0.0f;
 
-struct ann {
-        struct layer *hid, *out;
-        float *dtmp;
-        int batch_size;
-};      
-
-struct ann *ann_create(int f, int h, int cl, int bs)
-{
-        struct ann *a;
-        cudaMallocManaged(&a, sizeof(struct ann));
-        cudaMalloc((void **)&(a->dtmp), sizeof(float) * h * bs);
-        a->hid = layer_create(h, f, bs);
-        a->out = layer_create(cl, h, bs);
-        a->batch_size = bs;
-        return a;
-}
-
-void ann_destroy(struct ann *a)
-{
-        layer_destroy(a->out);
-        layer_destroy(a->hid);
-        cudaFree(a->dtmp);
-        cudaFree(a);
-}
-
 __device__ float drelu(float x)
 {
         return x > 0.0f ? 1.0f : 0.0f;
